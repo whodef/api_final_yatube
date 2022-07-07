1ceb768f9e075f4f010ba2fc237edbc890c77d58
whodef <tatyana-selyuk@yandex.com>
2022-07-07 21:23:27 +0300

    Almost done

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -101,7 +101,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
-
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
